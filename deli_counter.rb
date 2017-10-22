389c59834b0f2d3874c30a4ffd34002c940d04f6
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-22 16:00:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,5 +14,5 @@ end
 
 def take_a_number(array,name)
   array.push(name)
-  puts "Welcome, #{name}. You are number #{array.index(name)} in line."
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
 end
