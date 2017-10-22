49f2968b2b540bc6cf2b82e5abe276d4dd532f81
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-22 16:04:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,5 +18,8 @@ def take_a_number(array,name)
 end
 
 def now_serving(array)
-  puts "Currently serving #{array.shift}."
+  if array.size > 0
+    puts "Currently serving #{array.shift}."
+  else
+    puts "There is nobody waiting to be served!"
 end
