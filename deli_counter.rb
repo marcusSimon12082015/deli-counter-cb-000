ca6ed9c64f6971344adab116c6bba138961ce45c
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-22 15:44:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def line(array)
+  new_string = ""
+  if array.size == 0
+    return "The line is currently empty."
+  else
+    array.each_with_index{ |val,index|
+      new_string += "#{index+1}. #{val}"
+    }
+  end  
+  }
+  return "The line is currently: #{new_string}" 
+end
